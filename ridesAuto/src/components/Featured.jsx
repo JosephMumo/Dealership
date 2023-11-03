149b8896c47672aa643205b078ac71e6ab8f6688
JosephMumo <josephmumo25@gmail.com>
2023-11-03 15:46:29 +0300

    added cars component

--- ridesAuto/src/components/Featured.jsx
+++ ridesAuto/src/components/Featured.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useQuery } from 'react-query'
 import axios  from 'Axios'
+import {MdVerified} from 'react-icons/md'
 
 const Featured = () => {
     const getFeaturedCars = () => {
@@ -17,14 +18,20 @@ const Featured = () => {
   return (
     <div className='w-full h-auto p-12 md:p-14 bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:mt-0 mt-10 place-items-center'>
         { data?.data.map(item => {
-            return ( <div className='bg-white w-80 md:w-72 h-80 mt-5'
+            return ( <div className='bg-white w-80 md:w-72 h-80 mt-5 group cursor-pointer'
             key={item.id}>
                 <img src={item.img} alt='cars' className='w-80 md:w-72 h-1/2 object-cover' />
                 <section className='flex space-x-1 mx-3'>
                     <p>{item.year}</p>
                     <p>{item.title}</p>
                 </section>
-                <h3 className='mx-3 text-lg font-semibold'>{item.name}</h3>
+                <section className='flex justify-between items-center mx-4'>
+                    <h3 className='text-lg font-semibold'>{item.name}</h3>
+                    <section className='flex items-center space-x-1'>
+                        <p className='text-[#2ECC71] font-bold hidden group-hover:block'>Verified</p>
+                        <MdVerified size="1em" color="#2ECC71" className='hidden group-hover:block' />
+                    </section>
+                </section>
                 <section className='flex space-x-1 mx-3'>
                     <p>Milage:{item.milage} </p>
                     <p>{item.capacity}</p>
--- ridesAuto/src/pages/Home.jsx
+++ ridesAuto/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import Landing from '../components/Landing'
 import Featured from '../components/Featured'
 import Recent from '../components/Recent'
-import { appFunc } from '../context/AppContext'
 
 function Home() {
   const [ toggle, setToggle ]  = useState(true)
@@ -10,8 +9,8 @@ function Home() {
     <div className='bg-white'>
         <Landing />
         <div className='flex justify-center bg-blue-300 rounded-3xl w-72 relative mt-56 md:mt-10 mx-auto space-x-2 py-2'>
-          <button onClick={() => setToggle(true)} className={toggle ? 'py-1 px-4 bg-blue-600  text-basic rounded-3xl' : 'py-1 px-4 text-basic rounded-3xl'}>Featured Cars</button>
-          <button onClick={() => setToggle(false)} className={!toggle ? 'py-1 px-4 bg-blue-600 text-basic rounded-3xl' :'py-1 px-4 text-basic rounded-3xl'}>Recent Listing</button>
+          <button onClick={() => setToggle(true)} className={toggle ? 'py-1 px-4 bg-blue-600  rounded-3xl font-bold text-basic' : 'py-1 px-4 text-basic rounded-3xl'}>Featured Cars</button>
+          <button onClick={() => setToggle(false)} className={!toggle ? 'py-1 px-4 bg-blue-600 text-basic rounded-3xl font-bold' :'py-1 px-4 text-basic rounded-3xl'}>Recent Listing</button>
         </div>
         { toggle && <Featured /> }
         { !toggle && <Recent /> }
