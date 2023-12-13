742b5632ca5b2d4f946cb404f362bbfefaf5f160
JosephMumo <josephmumo25@gmail.com>
2023-12-14 00:08:09 +0300

    cart component remove item functionality

--- gariZen/src/components/Cart.jsx
+++ gariZen/src/components/Cart.jsx
@@ -20,7 +20,7 @@ const Cart = (props) => {
                             <br />
                             <h2 className='font-bold'>${item.price}</h2>
                         </section>
-                        <div className='w-[10%] grid place-items-center'>
+                        <div className='w-[10%] grid place-items-center' >
                             <MdDeleteOutline className='hover:scale-125 cursor-hover' size={24} color='black' />
                         </div>
                     </div>
