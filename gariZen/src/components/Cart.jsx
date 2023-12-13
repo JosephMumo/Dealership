62d83b43948e4bd3126a3bd36754db720eaeecf6
JosephMumo <josephmumo25@gmail.com>
2023-12-13 23:55:04 +0300

    added

--- gariZen/src/components/Cart.jsx
+++ gariZen/src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
 
 const Cart = (props) => {
     const { cart, setShowCart } = props
@@ -11,16 +12,23 @@ const Cart = (props) => {
             {cart.map(item => {
                 return(
                     <div key={item.id} className='border border-gray-200 h-42 w-[90%] md:w-[85%] lg:w-[75%] xl:w-[50%] flex'>
-                        <div className=' w-1/4'>
+                        <div className=' w-[25%]'>
                             <img className='object-scale-down w-32' src={item.imgUrl} alt='cart' />
                         </div>
-                        <section className=' w-3/4'>{item.name}</section>
+                        <section className=' w-[65%] py-5'>
+                            <p>{item.name}</p>
+                            <br />
+                            <h2 className='font-bold'>${item.price}</h2>
+                        </section>
+                        <div className='w-[10%] grid place-items-center'>
+                            <MdDeleteOutline className='hover:scale-110 cursor' size={24} color='black' />
+                        </div>
                     </div>
                 )
             })}
         </div>
-        <section className='flex items-center cursor-pointer' onClick={() => setShowCart(true)} >
-            <MdOutlineKeyboardBackspace size={24} color='black' className='animate-bounce' />
+        <section className='flex items-center cursor-pointer space-x-2' onClick={() => setShowCart(true)} >
+            <MdOutlineKeyboardBackspace size={24} color='black' className='animate-ping' />
             <p>Continue Shopping</p>
         </section>
     </div>
